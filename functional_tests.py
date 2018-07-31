0225b086941b649c2ac085c5e4a396ad2015cec4
EruDev <1027926875@qq.com>
2018-07-31 18:33:20 +0800

    functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 import unittest
 
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -18,6 +19,26 @@ class NewVisitorTest(unittest.TestCase):
 
         # 断言网页标题有 To-Do
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1')
+        self.assertIn('To-Do', header_text)
+
+        # 输入一个待办事项
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # 在文本输入框输入了一个"Buy peacock feathers"
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
         self.fail('Finish the test!')
 
 
